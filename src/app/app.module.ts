bdd6bca74e9501a90541a9cab008f58881600b31
cole <mctormey@outlook.com>
2023-05-29 14:03:41 -0700

    edited main page, changed tab icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WelcomeCardComponent } from './welcome/welcome-card/welcome-card.component';
+import { InterestsCardComponent } from './welcome/interests-card/interests-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeCardComponent,
+    InterestsCardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/welcome/interests-card/interests-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-interests-card',
+  template: "<h1>Here's a bunch of interesting interests</h1>",
+  styles: ['h1 {font-weight: normal;}']
+})
+
+export class InterestsCardComponent{
+
+}

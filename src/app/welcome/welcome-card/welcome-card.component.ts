39a4fec2d155b93c5585399d52046d4df4cce568
cole <mctormey@outlook.com>
2023-06-25 18:40:36 -0700

    added framework for three.js

--- src/app/welcome/welcome-card/welcome-card.component.ts
+++ src/app/welcome/welcome-card/welcome-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from "@angular/core";
+import * as THREE from "three";
 
 @Component({
   selector: 'app-welcome-card',
@@ -6,6 +7,89 @@ import { Component } from "@angular/core";
   styleUrls: ['welcome-card.component.css']
 })
 
-export class WelcomeCardComponent {
+export class WelcomeCardComponent implements AfterViewInit {
+  @ViewChild('canvas') public canvasRef: ElementRef;
+
+  // Menu Item Properties
+  @Input() public revolutionSpeed: number = 0.5;
+  @Input() public rotationSpeedX: number = 0.01;
+  @Input() public rotationSpeedY: number = 0.1;
+  @Input() public size: number = 64;
+  @Input() public orientation: number = 0;
+  @Input() public position: [number, number] = [0, 0]
+  @Input() public texture: string = "/assets/AC_menuBackground.png"
+
+  // Stage Properties
+  @Input() public cameraZ: number = 400;
+  @Input() public fieldOfView: number = 1;
+  @Input('nearClipping') public nearClippingPane: number = 1;
+  @Input('farClipping') public farClippingPane: number = 1000;
+
+  // Helper Properties (Private Properties)
+  private camera!: THREE.PerspectiveCamera;
+
+  private get canvas(): HTMLCanvasElement {
+    return this.canvasRef.nativeElement;
+  }
+
+  private loader = new THREE.TextureLoader();
+  private geometry = new THREE.BoxGeometry(1, 1, 1);
+  private material = new THREE.MeshPhongMaterial({ map: this.loader.load(this.texture) });
+
+  private cube: THREE.Mesh = new THREE.Mesh(this.geometry, this.material);
+  private renderer!: THREE.WebGLRenderer;
+  private scene!: THREE.Scene;
+
+  /* Create the scene */
+  private createScene() {
+    // scene
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.TextureLoader().load("/assets/AC_menuBackground.png");
+    this.scene.add(this.cube);
+
+    // camera
+    let aspectRatio = this.getAspectRatio();
+    this.camera = new THREE.PerspectiveCamera(
+      this.fieldOfView,
+      aspectRatio,
+      this.nearClippingPane,
+      this.farClippingPane
+    )
+    this.camera.position.z = this.cameraZ;
+  }
+
+  private getAspectRatio() {
+    return this.canvas.clientWidth / this.canvas.clientHeight;
+  }
+
+  // Animate Cube
+  private animateCube() {
+    this.cube.rotation.x += this.rotationSpeedX;
+    this.cube.rotation.y += this.rotationSpeedY;
+  }
+
+  // Begin Rendering Loop
+  private startRenderingLoop() {
+    // use canvas element in template
+    this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas });
+    this.renderer.setPixelRatio(devicePixelRatio);
+    this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
+
+    let component: WelcomeCardComponent = this;
+    (function render() {
+      requestAnimationFrame(render);
+      component.animateCube();
+      component.renderer.render(component.scene, component.camera);
+    }());
+  }
+
+  ngAfterViewInit() {
+    this.createScene();
+    this.startRenderingLoop();
+  }
+
+  ngOnInit() {
+
+  }
 
 }

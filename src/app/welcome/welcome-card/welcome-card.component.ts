9d73c2928a233f5e1f43b9a6bfedab0e5f7a5c23
cole <mctormey@outlook.com>
2023-06-28 10:20:59 -0700

    JEEEEESUS the raycasting is finally working

--- src/app/welcome/welcome-card/welcome-card.component.ts
+++ src/app/welcome/welcome-card/welcome-card.component.ts
@@ -34,6 +34,9 @@ export class WelcomeCardComponent implements AfterViewInit {
   private loader = new THREE.TextureLoader();
   private geometry = new THREE.BoxGeometry(1, 1, 1);
   private material = new THREE.MeshBasicMaterial({ map: this.loader.load(this.texture) });
+  private mouse = new THREE.Vector2();
+  private raycaster = new THREE.Raycaster();
+  private INTERSECTED: any;
 
   private cube1: THREE.Mesh = new THREE.Mesh(this.geometry, this.material);
 
@@ -65,6 +68,18 @@ export class WelcomeCardComponent implements AfterViewInit {
     return window.innerWidth / window.innerHeight;
   }
 
+  private hoverMenuItem() {
+    this.raycaster.setFromCamera(this.mouse, this.camera);
+    const intersects = this.raycaster.intersectObjects(this.scene.children, true);
+    if (intersects.length > 0) {
+      if (this.INTERSECTED !== intersects[0].object) { 
+        this.INTERSECTED = intersects[0].object;
+        this.INTERSECTED.rotation.x += 100;
+      } else 
+        this.INTERSECTED = null;
+    }
+  }
+
   // Animate 
   private animate() {
     this.cube1.rotation.x += this.rotationSpeedX;
@@ -79,16 +94,36 @@ export class WelcomeCardComponent implements AfterViewInit {
     this.camera.updateProjectionMatrix();
   }
 
+  // Locate Mouse
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    const pos = this.getCanvasRelativePosition(event);
+    this.mouse.x = (pos.x / this.canvas.width) * 2 - 1;
+    this.mouse.y = (pos.y / this.canvas.height) * -2 + 1;
+/*    this.cube1.rotation.y = 0;*/
+  }
+
+  getCanvasRelativePosition(event: MouseEvent) {
+    const rect = this.canvas.getBoundingClientRect();
+    console.log(rect);
+    return {
+      x: (event.clientX - rect.left) * this.canvas.width / rect.width,
+      y: (event.clientY - rect.top) * this.canvas.height / rect.height,
+    };
+  }
+
   // Begin Rendering Loop
   private startRenderingLoop() {
     // use canvas element in template
     this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas });
     this.renderer.setPixelRatio(window.devicePixelRatio);
-    this.renderer.setSize(window.innerWidth, window.innerHeight * 2/3);
-
+    this.renderer.setSize(window.innerWidth, window.innerHeight * 2 / 3);
+/*    this.canvas.appendChild(this.renderer.domElement);
+*/
     let component: WelcomeCardComponent = this;
     (function render() {
       requestAnimationFrame(render);
+      component.hoverMenuItem();
       component.renderer.render(component.scene, component.camera);
       component.animate();
     }());

c4dfd1713736dbab1891b249c689609450f63b90
cole <mctormey@outlook.com>
2023-06-27 19:20:56 -0700

    reorganizing code, prep for adding menu cyle animations

--- src/app/welcome/welcome-card/welcome-card.component.ts
+++ src/app/welcome/welcome-card/welcome-card.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild, HostListener } from "@angular/core";
 import * as THREE from "three";
 
+
 @Component({
   selector: 'app-welcome-card',
   templateUrl: 'welcome-card.component.html',
@@ -9,23 +10,7 @@ import * as THREE from "three";
 
 export class WelcomeCardComponent implements AfterViewInit {
   @ViewChild('canvas') public canvasRef: ElementRef;
-  @ViewChild('canvas_2') public canvasRef_2: ElementRef;
-
-  screenWidth: any;
-  screenHeight: any; 
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event: Event) {
-    this.screenWidth = window.innerWidth;
-    this.screenHeight = window.innerHeight;
-    this.camera.aspect = window.innerWidth / window.innerHeight;
-    this.camera.updateProjectionMatrix();
-
-    this.renderer.setSize(this.screenWidth, this.screenHeight * 3/4);
-
-  }
-
-
+  initialAspectRatio: any;
 
   // Menu Item Properties
   @Input() public revolutionSpeed: number = 0.5;
@@ -37,37 +22,36 @@ export class WelcomeCardComponent implements AfterViewInit {
   @Input() public texture: string = "/assets/raven.png"
 
   // Stage Properties
-  @Input() public cameraZ: number = 200;
+  @Input() public cameraZ: number = 400;
   @Input() public fieldOfView: number = 1;
   @Input('nearClipping') public nearClippingPane: number = 1;
   @Input('farClipping') public farClippingPane: number = 1000;
 
   // Helper Properties (Private Properties)
   private camera!: THREE.PerspectiveCamera;
-
-  private get canvas(): HTMLCanvasElement {
-    return this.canvasRef.nativeElement;
-  }
-
+  private renderer!: THREE.WebGLRenderer;
+  private scene!: THREE.Scene;
   private loader = new THREE.TextureLoader();
   private geometry = new THREE.BoxGeometry(1, 1, 1);
   private material = new THREE.MeshBasicMaterial({ map: this.loader.load(this.texture) });
 
   private cube1: THREE.Mesh = new THREE.Mesh(this.geometry, this.material);
-  private cube2: THREE.Mesh = new THREE.Mesh(this.geometry, this.material);
-  private renderer!: THREE.WebGLRenderer;
-  private scene!: THREE.Scene;
+
+  private get canvas(): HTMLCanvasElement {
+    return this.canvasRef.nativeElement;
+  }
 
   /* Create the scene */
   private createScene() {
     // scene
     this.scene = new THREE.Scene();
-    this.scene.background = new THREE.TextureLoader().load("/assets/AC_menuBackground.png");
+    this.scene.background = new THREE.TextureLoader().load("");
+/*    this.scene.background = new THREE.Color("black");*/
     this.scene.add(this.cube1);
-    this.scene.add(this.cube2);
 
     // camera
     let aspectRatio = this.getAspectRatio();
+    this.initialAspectRatio = aspectRatio;
     this.camera = new THREE.PerspectiveCamera(
       this.fieldOfView,
       aspectRatio,
@@ -78,24 +62,29 @@ export class WelcomeCardComponent implements AfterViewInit {
   }
 
   private getAspectRatio() {
-    return this.canvas.clientWidth / this.canvas.clientHeight;
+    return window.innerWidth / window.innerHeight;
   }
 
-  // Animate Cube
+  // Animate 
   private animate() {
     this.cube1.rotation.x += this.rotationSpeedX;
     this.cube1.rotation.y += this.rotationSpeedY;
+  }
 
-    this.cube2.rotation.x += 0.1;
-    this.cube2.position.x = -1;
+  // Resize
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.renderer.setSize(window.innerWidth, window.innerHeight * 2 / 3);
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
   }
 
   // Begin Rendering Loop
   private startRenderingLoop() {
     // use canvas element in template
     this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas });
-    this.renderer.setPixelRatio(devicePixelRatio);
-    this.renderer.setSize(this.canvas.clientWidth, window.innerHeight * 3/4);
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth, window.innerHeight * 2/3);
 
     let component: WelcomeCardComponent = this;
     (function render() {

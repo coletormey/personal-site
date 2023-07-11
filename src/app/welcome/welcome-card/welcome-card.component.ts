962d018132e66f9f965f31fe7d29ab43a7f688d5
cole <mctormey@outlook.com>
2023-07-11 14:27:54 -0700

    ROTATION OF MENU ITEMS LEFT AND RIGHT FINALLY FUCKING  WORKS

--- src/app/welcome/welcome-card/welcome-card.component.ts
+++ src/app/welcome/welcome-card/welcome-card.component.ts
@@ -32,7 +32,9 @@ export class WelcomeCardComponent {
   // Helper Properties (Private Properties)
   private scene!: THREE.Scene;
   private sceneBackgroundTexture: string = "/assets/AC_menuBackground.png";
-
+  private clock = new THREE.Clock();
+  private speed = 1; //units a second
+  private delta = 0;
   private camera!: THREE.PerspectiveCamera;
   private renderer!: THREE.WebGLRenderer;
   private labelRenderer!: CSS2DRenderer;
@@ -50,8 +52,13 @@ export class WelcomeCardComponent {
   private leftMenuArrow!: THREE.Mesh;
   private rightMenuArrow!: THREE.Mesh;
   private menuOptionCircle!: THREE.Line;
+  private rotateMenuRight: boolean = false;
+  private rotateMenuLeft: boolean = false;
   private INTERSECTED: any;
-  private listOfSceneObjects = [];
+  private listOfMenuObjects: Flow[] = [];
+  private menuOptionsLocation = [0, 0.25, 0.75];
+  private menuOptionsFlowSpeedPerPosition_Left = [-0.01, -0.01, -0.02];
+  private menuOptionsFlowSpeedPerPosition_Right = [0.01, 0.02, 0.01];
 
 
   ngOnInit() {
@@ -70,7 +77,6 @@ export class WelcomeCardComponent {
     this.raycaster = new THREE.Raycaster();
     this.labelRenderer = new CSS2DRenderer();
     this.createSceneObjects();
-
   }
 
 
@@ -91,7 +97,7 @@ export class WelcomeCardComponent {
     const geometry = new THREE.BufferGeometry().setFromPoints(points);
     const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
     this.menuOptionCircle = new THREE.Line(geometry, material);
-    this.scene.add(this.menuOptionCircle);
+/*    this.scene.add(this.menuOptionCircle);*/
 
     const cube1 = new THREE.BoxGeometry(1, 1, 1);
     const cube2 = new THREE.BoxGeometry(1, 1, 1);
@@ -102,7 +108,9 @@ export class WelcomeCardComponent {
     this.flow_profileOption = new Flow(this.menuOptionMesh_profile);
     this.flow_profileOption.updateCurve(0, this.curve);
     this.flow_profileOption.uniforms.flow.value = false;
+    this.listOfMenuObjects.push(this.flow_profileOption);
     this.scene.add(this.flow_profileOption.object3D);
+
 
     const servicesMaterial = new THREE.MeshBasicMaterial({ color: 0xC0C0C0 });
     this.menuOptionMesh_services = new THREE.Mesh(cube2, servicesMaterial);
@@ -110,7 +118,9 @@ export class WelcomeCardComponent {
     this.flow_servicesOption.updateCurve(0, this.curve);
     this.flow_servicesOption.uniforms.flow.value = false;
     this.flow_servicesOption.uniforms.pathOffset.value = 0.25;
+    this.listOfMenuObjects.push(this.flow_servicesOption);
     this.scene.add(this.flow_servicesOption.object3D);
+
 
     const systemMaterial = new THREE.MeshBasicMaterial({ color: 0xBB0000 });
     this.menuOptionMesh_system = new THREE.Mesh(cube3, systemMaterial);
@@ -118,7 +128,25 @@ export class WelcomeCardComponent {
     this.flow_systemOption.updateCurve(0, this.curve);
     this.flow_systemOption.uniforms.flow.value = false;
     this.flow_systemOption.uniforms.pathOffset.value = 0.75;
+    this.listOfMenuObjects.push(this.flow_systemOption);
     this.scene.add(this.flow_systemOption.object3D);
+
+
+    const arrowMaterial = new THREE.MeshNormalMaterial();
+    const arrowGeometry = new THREE.BufferGeometry();
+    const arrowPoints = [
+      new THREE.Vector3(-0.25, 0.25, 0),
+      new THREE.Vector3(-0.25, -0.25, 0),
+      new THREE.Vector3(0.25, -0.25, 0)
+    ];
+    arrowGeometry.setFromPoints(arrowPoints);
+    arrowGeometry.computeVertexNormals();
+    this.rightMenuArrow = new THREE.Mesh(arrowGeometry, arrowMaterial);
+    this.rightMenuArrow.rotateX(-30);
+    this.rightMenuArrow.position.setX(2);
+    this.leftMenuArrow = this.rightMenuArrow.clone();
+    this.leftMenuArrow.position.setX(-1.75);
+    this.scene.add(this.rightMenuArrow, this.leftMenuArrow);
 
   }
 
@@ -141,6 +169,7 @@ export class WelcomeCardComponent {
     let component: WelcomeCardComponent = this;
     (function render() {
       requestAnimationFrame(render);
+      component.delta = component.clock.getDelta();
       component.hoverMenuItem();
       component.renderer.render(component.scene, component.camera);
       component.labelRenderer.render(component.scene, component.camera);
@@ -150,12 +179,37 @@ export class WelcomeCardComponent {
 
 
   private animate() {
-
-  }
-
-
-  private getAspectRatio() {
-    return window.innerWidth / (window.innerHeight * 2 / 3);
+    if (this.rotateMenuLeft && Math.round((this.listOfMenuObjects[0].uniforms.pathOffset.value + Number.EPSILON) * 100) / 100 !== -0.25) {
+      this.listOfMenuObjects[0].moveAlongCurve(-0.01);
+      this.listOfMenuObjects[1].moveAlongCurve(-0.01);
+      this.listOfMenuObjects[2].moveAlongCurve(-0.02);
+    } else if (this.rotateMenuRight && Math.round((this.listOfMenuObjects[0].uniforms.pathOffset.value + Number.EPSILON) * 100) / 100 !== 0.25) {
+      this.listOfMenuObjects[0].moveAlongCurve(0.01);
+      this.listOfMenuObjects[1].moveAlongCurve(0.02);
+      this.listOfMenuObjects[2].moveAlongCurve(0.01);
+    } else {
+      if (this.rotateMenuLeft) {
+        this.rotateMenuLeft = false;
+        let temp = this.listOfMenuObjects[0];
+        for (let i = 0; i <= this.listOfMenuObjects.length - 1; i++) {
+          if (i == this.listOfMenuObjects.length - 1)
+            this.listOfMenuObjects[i] = temp;
+          else
+            this.listOfMenuObjects[i] = this.listOfMenuObjects[i + 1];
+          this.listOfMenuObjects[i].uniforms.pathOffset.value = this.menuOptionsLocation[i];
+        }
+      } else if (this.rotateMenuRight) {
+        this.rotateMenuRight = false;
+        let temp = this.listOfMenuObjects[2];
+        for (let i = this.listOfMenuObjects.length - 1; i >= 0; i--) {
+          if (i == 0)
+            this.listOfMenuObjects[i] = temp;
+          else
+            this.listOfMenuObjects[i] = this.listOfMenuObjects[i - 1];
+          this.listOfMenuObjects[i].uniforms.pathOffset.value = this.menuOptionsLocation[i];
+        }
+      }
+    }
   }
 
   // Hover
@@ -165,12 +219,12 @@ export class WelcomeCardComponent {
     if (intersects.length > 0 ) {
         this.INTERSECTED = intersects[0].object;
 
-      
+     
     }
   }
 
 
-  // Resize
+  // Resize Listener
   @HostListener('window:resize', ['$event'])
   onResize(event: Event) {
     this.renderer.setSize(window.innerWidth, window.innerHeight * 2 / 3);
@@ -189,7 +243,7 @@ export class WelcomeCardComponent {
     }
   }
 
-  // Locate Mouse
+  // Locate Mouse Listener
   @HostListener('mousemove', ['$event'])
   onMouseMove(event: MouseEvent) {
     const pos = this.getCanvasRelativePosition(event);
@@ -204,14 +258,22 @@ export class WelcomeCardComponent {
     };
   }
 
-  // Click Event
-/*  @HostListener('click', ['$event'])
+  // Click Event Listener
+  @HostListener('click', ['$event'])
   onClick(event: MouseEvent) {
     this.raycaster.setFromCamera(this.mouse, this.camera);
     let intersects = this.raycaster.intersectObjects(this.scene.children, true);
-    let temp: any;
     if (intersects.length > 0) {
       this.INTERSECTED = intersects[0].object;
+      if (this.INTERSECTED == this.leftMenuArrow) {
+        this.rotateMenuLeft = true;
+      } else if (this.INTERSECTED == this.rightMenuArrow) {
+        this.rotateMenuRight = true;
+      }
     }
-  }*/
+  }
+
+  private getAspectRatio() {
+    return window.innerWidth / (window.innerHeight * 2 / 3);
+  }
 }
